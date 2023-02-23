93d82130a1395e838c83ea8220e6d86f4aef7bf1
Dionison <krekmg@gmail.com>
2023-02-23 11:17:54 -0300

    nest

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,12 @@
 import { ReactNode } from "react";
-
+import "./globals.css";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <head />
 
-      <body>
-          {children}
-      </body>
+      <body className="bg-gray-900 px-5 py-5">{children}</body>
     </html>
   );
 }
